196d86493456e6d54be3a44af58b30f0371082e6
Pierre Maillot <maillpierre@gmail.com>
2023-12-12 10:59:14 +0100

    update to integrate Indegx sparql endpoints

--- update-status.sh
+++ update-status.sh
@@ -6,12 +6,14 @@
 # TODO Update lod cloud status once the PR is accepted
 # wget -O LODCloud_SPARQL_Endpoints.ttl https://raw.githubusercontent.com/OpenLinkSoftware/general-turtle-doc-collection/master/LODCloud_SPARQL_Endpoints.ttl
 
+# Download the IndeGx catalog of endpoints
+[ -f indegx.ttl ] || wget -O indegx.ttl https://raw.githubusercontent.com/Wimmics/IndeGx/main/catalogs/all_catalog_edited.ttl
 # Map each endpoint to its own named graph,
+java -cp si.jar sparqlintegrate LODCloud_SPARQL_Endpoints.ttl indegx.ttl 'PREFIX sd: <http://www.w3.org/ns/sparql-service-description#> PREFIX void: <http://rdfs.org/ns/void#> CONSTRUCT { GRAPH ?o { ?s ?p ?o } } { ?s ?p ?x FILTER(?p = sd:endpoint || ?p = void:sparqlEndpoint) BIND(IRI(REPLACE(STR(?x), "\\s+", "")) AS ?o) }' | \
 # run the status check for each named graph in parallel
-# and then merge the results
-java -cp si.jar sparqlintegrate LODCloud_SPARQL_Endpoints.ttl 'CONSTRUCT { GRAPH ?o { ?s ?p ?o } } { ?s ?p ?x FILTER(?p = sd:endpoint) BIND(IRI(REPLACE(STR(?x), "\\s+", "")) AS ?o) }' | \
 java -cp si.jar ngs map -t '5000,5000' --sparql status-check.sparql | \
 java -cp si.jar sparqlintegrate --w=trig/pretty --u - 'PREFIX schema: <https://schema.org/> CONSTRUCT WHERE { ?s ?p ?o } ORDER BY ?s ?p ?o' > next-status.ttl
+# and then merge the results
 
 ./diff-status.sh latest-status.ttl next-status.ttl > effective-status.ttl
 
