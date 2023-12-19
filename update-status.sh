913015119b77fd858b88d8ba2a23244b0b2c4f5a
Pierre Maillot <maillpierre@gmail.com>
2023-12-19 14:57:43 +0100

    Revert "Removing Openlink endpoint list, all endpoint already known"
    This reverts commit bb740686d29d256b2e7cc1ae4f860f4b95458d79.

--- update-status.sh
+++ update-status.sh
@@ -3,10 +3,13 @@
 # Download SPARQL Integrate (if not already available)
 [ -f si.jar ] || wget -O si.jar https://github.com/SmartDataAnalytics/SparqlIntegrate/releases/download/sparql-integrate-bundle-1.0.3-SNAPSHOT/sparql-integrate-bundle-1.0.3-SNAPSHOT-jar-with-dependencies.jar
 
+# TODO Update lod cloud status once the PR is accepted
+# wget -O LODCloud_SPARQL_Endpoints.ttl https://raw.githubusercontent.com/OpenLinkSoftware/general-turtle-doc-collection/master/LODCloud_SPARQL_Endpoints.ttl
+
 # Download the IndeGx catalog of endpoints
 [ -f indegx.ttl ] || wget -O indegx.ttl https://raw.githubusercontent.com/Wimmics/IndeGx/main/catalogs/all_catalog_edited.ttl
 # Map each endpoint to its own named graph,
-java -cp si.jar sparqlintegrate indegx.ttl 'PREFIX sd: <http://www.w3.org/ns/sparql-service-description#> PREFIX void: <http://rdfs.org/ns/void#> PREFIX dcat: <http://www.w3.org/ns/dcat#> CONSTRUCT { GRAPH ?o { ?s ?p ?o } } { ?s ?p ?x FILTER(?p = sd:endpoint || ?p = void:sparqlEndpoint || ?p = dcat:endpointURL ) BIND(IRI(REPLACE(STR(?x), "\\s+", "")) AS ?o) }' | \
+java -cp si.jar sparqlintegrate LODCloud_SPARQL_Endpoints.ttl indegx.ttl 'PREFIX sd: <http://www.w3.org/ns/sparql-service-description#> PREFIX void: <http://rdfs.org/ns/void#> PREFIX dcat: <http://www.w3.org/ns/dcat#> CONSTRUCT { GRAPH ?o { ?s ?p ?o } } { ?s ?p ?x FILTER(?p = sd:endpoint || ?p = void:sparqlEndpoint || ?p = dcat:endpointURL ) BIND(IRI(REPLACE(STR(?x), "\\s+", "")) AS ?o) }' | \
 # run the status check for each named graph in parallel
 java -cp si.jar ngs map -t '5000,5000' --sparql status-check.sparql | \
 java -cp si.jar sparqlintegrate --w=trig/pretty --u - 'PREFIX schema: <https://schema.org/> CONSTRUCT WHERE { ?s ?p ?o } ORDER BY ?s ?p ?o' > next-status.ttl
